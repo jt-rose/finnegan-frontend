9247eb6802a4efb969aa341b00919c6cb383c548
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-07 00:59:55 -0400

    set up working login mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Index from "./pages/Index";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Navbar />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Navbar />
+        <Index />
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+import { useMutation } from "react-query";
+import axios from "axios";
+
+function Index() {
+  //const router = useRouter();
+  const loginMutation = useMutation(
+    (config: { username: string; password: string }) =>
+      axios.post("http://localhost:8080/login", config),
+    {
+      onSuccess: (data) => {
+        const token = data.headers.authorization;
+        console.log(token);
+        //sessionStorage.setItem("token", token);
+        //router.push("/");
+      },
+    }
+  );
+
+  const handleLogin = () => {
+    loginMutation.mutate({ username: "user", password: "password" });
+  };
+
+  return <p onClick={handleLogin}>login</p>;
+}
+
+export default Index;
